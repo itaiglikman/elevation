e6d15443701317f262ce1a9e110070e3f35192d5
itaiglikman <itaig1998@gmail.com>
2025-07-10 18:03:01 +0300

    fix: format code and improve swap function implementation

--- 02-JS-and-Algo/09-recursions/index.js
+++ 02-JS-and-Algo/09-recursions/index.js
@@ -18,8 +18,8 @@ const findFactorial = function (num) {
 // Reverse a string using recursion (no loops, of course).
 const reverseString = function (str) {
     console.log(str);
-    if(str.length===1) return str;
-    return str.slice(-1)+reverseString(str.slice(0,-1));
+    if (str.length === 1) return str;
+    return str.slice(-1) + reverseString(str.slice(0, -1));
 }
 let str = "abcd"
 // console.log(reverseString(str));
@@ -31,16 +31,11 @@ let str = "abcd"
 const arr1 = [1, 2, 3]
 const arr2 = []
 
-const swap = function (arr1,arr2) {
-    //Your code here
-    if(arr1.length===1) return arr1.pop();
-    let popped = arr1.pop();
-    return swap(arr1,arr2.push(popped))
+const swap = function (arr1, arr2) {
+    if (arr1.length === 0) return
+    let first = arr1.shift();
+    arr2.push(first);
+    return swap(arr1, arr2)
 }
 
-swap(arr1, arr2)
-console.log(arr1) //[]
-// console.log(arr1) //[]
-console.log(arr2) //[1, 2, 3]
-/* DO NOT REMOVE THE EXPORTS BELOW */
-// module.exports = { findFactorial, reverseString, swap }
+swap(arr1, arr2);
