0a9ca8222fe5da11f2ad6072bbf749a3232aad66
itaiglikman <itaig1998@gmail.com>
2025-07-27 17:17:34 +0300

    implement post model

--- 03-JS-and-Web/tweeter-ui/models/post.js
+++ 03-JS-and-Web/tweeter-ui/models/post.js
@@ -1,5 +1,58 @@
-class Post{
+import validate from '../Utils/validate.js';
+class Post {
 
+    constructor(id, text) {
+        this.text = text;
+        this.id = id;
+        this.comments = [];
+    }
+
+    set id(value) {
+        validate.validatePostId(value);
+        this._id = value;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set text(value) {
+        validate.validatePostText(value);
+        this._text = value;
+    }
+
+    get text() {
+        return this._text;
+    }
+
+    addComment(comment) {
+        try {
+            this.comments.push(comment);
+        } catch (error) {
+            console.error(error.message);
+            // throw new Error(error);
+            throw error;
+        }
+    }
+
+    removeComment(commentId) {
+        try {
+            const commentIndex = this.#checkCommentExists(commentId);
+            this.comments.splice(commentIndex, 1);
+            console.log(`post ${commentIndex} removed`);
+        } catch (error) {
+            console.error(error.message);
+            // throw new Error(error)
+            throw error;
+        }
+    }
+
+    #checkCommentExists(commentId) {
+        validate.validateCommentId(commentId);
+        const index = this.comments.findIndex(c => c.id === commentId);
+        if (index === -1) throw (`comment ${commentId} not found`);
+        return index;
+    }
 }
 
 export default Post;
