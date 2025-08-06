518c30810773c1789f8c07b5d73170a9543246bd
itaiglikman <itaig1998@gmail.com>
2025-08-06 14:03:44 +0300

    movies

--- 04-React/movies/src/Components/Movie/Movie.tsx
+++ 04-React/movies/src/Components/Movie/Movie.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
 import "./Movie.css";
 
 export type MovieProps = {
@@ -14,12 +15,18 @@ export type MovieProps = {
 };
 
 export function Movie({ movie }: MovieProps) {
-  const { name, runtime, year, plot, genres, poster, imdb } = movie;
+  let { name, runtime, year, plot, genres, poster, imdb } = movie;
+
+  const [nameValue, setName] = useState(name);
 
   function getTime(): string {
     const hours = Math.floor(runtime / 60);
     const minutes = runtime - hours * 60;
     return hours + ":" + minutes;
+  }
+
+  function handleClick(genre:string) {
+    setName(genre);
   }
 
   return (
@@ -28,15 +35,14 @@ export function Movie({ movie }: MovieProps) {
         <img src={poster} />
       </div>
       <div className="info">
-        <div>{name}</div>
-        <div>{name}</div>
+        <div>{nameValue}</div>
         <div>
           {year} | {getTime()}
         </div>
         <div>{plot}</div>
         <div className="genres">
           {genres.map((g) => (
-            <div>{g}</div>
+            <div onClick={()=>handleClick(g)}>{g}</div>
           ))}
         </div>
         <a href={imdb} target="_blank">
--- 04-React/movies/src/Components/MoviesPage/MoviesPage.tsx
+++ 04-React/movies/src/Components/MoviesPage/MoviesPage.tsx
@@ -1,44 +1,14 @@
 import { Movie } from "../Movie/Movie";
 import "./MoviesPage.css";
+import jsonMovies from "../../data/movies.json";
 
 export function MoviesPage() {
-  const movies = [
-    {
-      name: "Forrest Gump",
-      runtime: 142,
-      year: 1994,
-      plot: "The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
-      genres: ["Drama", "Comedy"],
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
-      imdb: "https://www.imdb.com/title/tt0109830/",
-    },
-    {
-      name: "Inception",
-      runtime: 148,
-      year: 2010,
-      plot: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
-      genres: ["Action", "Sci-Fi", "Thriller"],
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/7/7f/Inception_ver3.jpg",
-      imdb: "https://www.imdb.com/title/tt1375666/",
-    },
-    {
-      name: "The Shawshank Redemption",
-      runtime: 142,
-      year: 1994,
-      plot: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-      genres: ["Drama"],
-      poster:
-        "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
-      imdb: "https://www.imdb.com/title/tt0111161/",
-    },
-  ];
+  const movies = jsonMovies;
 
   return (
     <div className="MoviesPage">
-      {movies.map((m, index) => (
-        <Movie movie={m} key={index} />
+      {movies.map((m) => (
+        <Movie movie={m} key={m.id} />
       ))}
     </div>
   );
