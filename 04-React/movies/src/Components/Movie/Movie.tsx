019729d821f54932b26a482c2cae507a90d78828
itaiglikman <itaig1998@gmail.com>
2025-08-10 12:54:58 +0300

    state lifting

--- 04-React/movies/src/Components/Movie/Movie.tsx
+++ 04-React/movies/src/Components/Movie/Movie.tsx
@@ -2,8 +2,7 @@
 import { useState } from "react";
 import "./Movie.css";
 
-export type MovieProps = {
-  movie: {
+export default interface MovieType {
     name: string;
     runtime: number;
     year: number;
@@ -11,44 +10,49 @@ export type MovieProps = {
     genres: Array<string>;
     poster: string;
     imdb: string;
-  };
+}
+
+export type MovieProps = {
+    movie: MovieType;
+    isSelected: boolean;
+    onSelect: (name: string) => void
 };
 
-export function Movie({ movie }: MovieProps) {
-  let { name, runtime, year, plot, genres, poster, imdb } = movie;
+export function Movie({ movie, isSelected, onSelect }: MovieProps) {
+    let { name, runtime, year, plot, genres, poster, imdb } = movie;
 
-  const [nameValue, setName] = useState(name);
+    const [nameValue, setName] = useState(name);
 
-  function getTime(): string {
-    const hours = Math.floor(runtime / 60);
-    const minutes = runtime - hours * 60;
-    return hours + ":" + minutes;
-  }
+    function getTime(): string {
+        const hours = Math.floor(runtime / 60);
+        const minutes = runtime - hours * 60;
+        return hours + ":" + minutes;
+    }
 
-  function handleClick(genre:string) {
-    setName(genre);
-  }
+    function handleClick(genre: string) {
+        setName(genre);
+    }
 
-  return (
-    <div className="Movie">
-      <div className="movie-poster">
-        <img src={poster} />
-      </div>
-      <div className="info">
-        <div>{nameValue}</div>
-        <div>
-          {year} | {getTime()}
+    return (
+        <div className={'Movie' + (isSelected ? ' selected' : '')}>
+            <div className="movie-poster" onClick={() => onSelect(movie.name)}>
+                <img src={poster} />
+            </div>
+            <div className="info">
+                <div>{nameValue}</div>
+                <div>
+                    {year} | {getTime()}
+                </div>
+                <div>{plot}</div>
+                <div className="genres">
+                    {genres.map((g) => (
+                        <div onClick={() => handleClick(g)}>{g}</div>
+                    ))}
+                </div>
+                <a href={imdb} target="_blank">
+                    link to imdb
+                </a>
+            </div>
         </div>
-        <div>{plot}</div>
-        <div className="genres">
-          {genres.map((g) => (
-            <div onClick={()=>handleClick(g)}>{g}</div>
-          ))}
-        </div>
-        <a href={imdb} target="_blank">
-          link to imdb
-        </a>
-      </div>
-    </div>
-  );
+    );
 }
--- 04-React/movies/src/Components/MoviesPage/MoviesPage.tsx
+++ 04-React/movies/src/Components/MoviesPage/MoviesPage.tsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
+import jsonMovies from "../../data/movies.json";
 import { Movie } from "../Movie/Movie";
 import "./MoviesPage.css";
-import jsonMovies from "../../data/movies.json";
 
 export function MoviesPage() {
-  const movies = jsonMovies;
+    const [movies, setMovies] = useState(jsonMovies);
+    const [comedyOnly, setComedyOnly] = useState(false);
+    const [filterText, setFilterText] = useState("");
+    const [selectedMovieName, setSelectedMovieName] = useState('');
 
-  return (
-    <div className="MoviesPage">
-      {movies.map((m) => (
-        <Movie movie={m} key={m.id} />
-      ))}
-    </div>
-  );
+
+    let displayedMovies = movies;
+
+    const filterByComedy = () => movies.filter(m => m.genres.includes('Comedy'));
+    const filterByText = () => displayedMovies.filter(movie =>
+        movie.name.toLowerCase().includes(filterText.toLowerCase()));
+
+    if (comedyOnly) displayedMovies = filterByComedy();
+    if (filterText) displayedMovies = filterByText();
+    
+    function handleSelectedMovie(movieName: string) {
+        movieName === selectedMovieName
+            ? setSelectedMovieName('')
+            : setSelectedMovieName(movieName);
+    }
+
+    return (
+        <div className="MoviesPage">
+            <h1>Movies page</h1>
+            <div className="filter-movies">
+                <input
+                    type="text"
+                    placeholder="Filter movies"
+                    value={filterText}
+                    onChange={(e) => setFilterText(e.target.value)} />
+                <button onClick={() => setComedyOnly(!comedyOnly)}>{comedyOnly ? 'Show all' : 'Comedy only'}</button>
+            </div>
+            {displayedMovies.map((m) => (
+                <Movie movie={m} key={m.name}
+                    isSelected={selectedMovieName === m.name}
+                    onSelect={handleSelectedMovie} />
+            ))}
+        </div>
+    );
 }
