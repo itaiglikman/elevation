a7b8d705e558547d3602ae9000f73f28118b2b6f
itaiglikman <itaig1998@gmail.com>
2025-09-01 16:53:06 +0300

    pokecorp initial commit

---
+++ 05-Backend/SQL/sequelize/PokeCorp/server.ts
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+import cors from "cors";
+import express from "express";
+import expressRateLimit from "express-rate-limit";
+import catchAll from "./03-middlewares/catch-all";
+import logger from "./03-middlewares/logger";
+
+// Load environment variables from .env file
+dotenv.config();
+
+const server = express();
+
+// security DoS Attack: limits number of request from the same IP:
+server.use(expressRateLimit({
+    windowMs: 1000, //time limit
+    max: 20 //max requests allowed in that time window
+}));
+
+server.use(express.json());
+server.use(cors());
+server.use(logger);
+
+server.use('/api')
+
+server.use(catchAll);
+
+server.listen(process.env.PORT, () => {
+    console.log('listening on port:' + process.env.PORT);
+})
