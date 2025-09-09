bba0a020317fe683c4c64759e27a9210572a6d05
itaiglikman <itaig1998@gmail.com>
2025-09-09 18:46:04 +0300

    mongoose and query exercises

--- 05-Backend/06-Mongo/exercises/mongoose-and-query/models/BookModel.js
+++ 05-Backend/06-Mongo/exercises/mongoose-and-query/models/BookModel.js
@@ -1,1 +1,15 @@
-console.log('some change');
+//BookModel.js
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const bookSchema = new Schema({ 
+  title: String,
+  author: String,
+  pages: Number,
+  genres: [String],
+  rating: Number
+})
+
+const Book = mongoose.model("Book", bookSchema)
+module.exports = Book
--- 05-Backend/06-Mongo/exercises/mongoose-and-query/server.js
+++ 05-Backend/06-Mongo/exercises/mongoose-and-query/server.js
@@ -11,11 +11,12 @@ var mongoose = require('mongoose')
 var Book = require("./models/BookModel")
 var Person = require("./models/PersonModel")
 
-mongoose.connect("mongodb://127.0.0.1:27017/mongoose-practice", {
-  useNewUrlParser: true,
+const dbConnectionString = `mongodb+srv://itaig1998_db_user:<password>@cluster0.oypbsmo.mongodb.net/Query?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose.connect(dbConnectionString, {
+    useNewUrlParser: true,
 })
-.then(()=>console.log("conneted to DB"))
-.catch((err)=> console.log(err))
+    .then(() => console.log("conneted to DB"))
+    .catch((err) => console.log(err))
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -28,42 +29,42 @@ Create books Collection
 var isbns = [9780156012195, 9780743273565, 9780435905484, 9780140275360, 9780756404741, 9780756407919, 9780140177398, 9780316769488, 9780062225672, 9780143130154, 9780307455925, 9781501143519]
 var url = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
 
-for (var i = 0; i < isbns.length; i++) {
-  var apiURL = url + isbns[i];
-  /*=====================================================
-  the first time you run your code, uncomment the function below.
-  for subsequent runs, re-comment it so that it runs only once!
-  that said, there is a fail-safe to avoid duplicates below
-  =======================================================*/
-  loadFromAPI(apiURL)
-}
+// for (var i = 0; i < isbns.length; i++) {
+//   var apiURL = url + isbns[i];
+//   /*=====================================================
+//   the first time you run your code, uncomment the function below.
+//   for subsequent runs, re-comment it so that it runs only once!
+//   that said, there is a fail-safe to avoid duplicates below
+//   =======================================================*/
+//   loadFromAPI(apiURL)
+// }
 console.log("done");
 
 function loadFromAPI(apiURL) {
 
-  request(apiURL, function(error, response, body) {
+    request(apiURL, function (error, response, body) {
 
-    var result = JSON.parse(body)
+        var result = JSON.parse(body)
 
-    if (result.totalItems && !error && response.statusCode == 200) {
-      var resBook = JSON.parse(body).items[0].volumeInfo
+        if (result.totalItems && !error && response.statusCode == 200) {
+            var resBook = JSON.parse(body).items[0].volumeInfo
 
-      var book = new Book({
-        title: resBook.title,
-        author: resBook.authors ? resBook.authors[0] : '',
-        pages: resBook.pageCount,
-        genres: resBook.categories || ["Other"],
-        rating: resBook.averageRating || 5
-      })
+            var book = new Book({
+                title: resBook.title,
+                author: resBook.authors ? resBook.authors[0] : '',
+                pages: resBook.pageCount,
+                genres: resBook.categories || ["Other"],
+                rating: resBook.averageRating || 5
+            })
 
-      //Only save if the book doesn't exist yet
-      Book.findOne({ title: book.title }).then( function(err, foundBook) {
-        if (!foundBook) {
-          book.save()
+            //Only save if the book doesn't exist yet
+            Book.findOne({ title: book.title }).then(function (err, foundBook) {
+                if (!foundBook) {
+                    book.save()
+                }
+            })
         }
-      })
-    }
-  })
+    })
 }
 
 
@@ -71,37 +72,37 @@ function loadFromAPI(apiURL) {
 Create People Collection
 =======================================================*/
 var colors = ["brown", "black", "red", "yellow", "green", "grey"]
-var getColor = function() {
-  return colors[Math.floor(Math.random() * colors.length)]
+var getColor = function () {
+    return colors[Math.floor(Math.random() * colors.length)]
 }
-var getWeight = function() {
-  return getRandIntBetween(50, 120)
+var getWeight = function () {
+    return getRandIntBetween(50, 120)
 }
-var getHeight = function() {
-  return getRandIntBetween(120, 230)
+var getHeight = function () {
+    return getRandIntBetween(120, 230)
 }
-var getSalary = function() {
-  return getRandIntBetween(20000, 50000)
+var getSalary = function () {
+    return getRandIntBetween(20000, 50000)
 }
-var getNumKids = function() {
-  return Math.floor(Math.random() * 3)
-}
-
-var getRandIntBetween = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min)
+var getNumKids = function () {
+    return Math.floor(Math.random() * 3)
 }
 
-var getKids = function(numKids) {
-  var kids = [];
-  for (var i = 0; i < numKids; i++) {
-    kids.push({
-      hair: getColor(),
-      eyes: getColor(),
-      weight: getWeight(),
-      height: getHeight(),
-    })
-  }
-  return kids;
+var getRandIntBetween = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+var getKids = function (numKids) {
+    var kids = [];
+    for (var i = 0; i < numKids; i++) {
+        kids.push({
+            hair: getColor(),
+            eyes: getColor(),
+            weight: getWeight(),
+            height: getHeight(),
+        })
+    }
+    return kids;
 }
 
 
@@ -113,33 +114,33 @@ adds new people and their kids until you do have 100
 try to understand how this code works
 could you write it differently?
 =======================================================*/
-Person.find({}).count().then(function(err, count) {
-  // the below two loops could be changed to a simple:
-  // for (var i = count; i < 100; i++) {}
-  if (count < 100) {
-    for (var i = 0; i < 100 - count; i++) {
-      var numKids = getNumKids();
-      var p = new Person({
-        hair: getColor(),
-        eyes: getColor(),
-        weight: getWeight(),
-        height: getHeight(),
-        salary: getSalary(),
-        numKids: numKids,
-        kids: getKids(numKids)
-      });
-      p.save();
-    }
-  }
-})
+// Person.find({}).count().then(function (count) {
+//     // the below two loops could be changed to a simple:
+//     // for (var i = count; i < 100; i++) {}
+//     if (count < 100) {
+//         for (var i = 0; i < 100 - count; i++) {
+//             var numKids = getNumKids();
+//             var p = new Person({
+//                 hair: getColor(),
+//                 eyes: getColor(),
+//                 weight: getWeight(),
+//                 height: getHeight(),
+//                 salary: getSalary(),
+//                 numKids: numKids,
+//                 kids: getKids(numKids)
+//             });
+//             p.save();
+//         }
+//     }
+// })
 
 
 /*=====================================================
 Start the server:
 =======================================================*/
 
-app.listen(3000, function() {
-  console.log("Server up and running on port 3000")
+app.listen(3000, function () {
+    console.log("Server up and running on port 3000")
 })
 
 
@@ -155,20 +156,87 @@ and your server is running do the following:
 /*Books
 ----------------------*/
 //1. Find books with fewer than 500 but more than 200 pages
+async function ex1() {
+    const books = await Book.find({ pages: { $gt: 200, $lt: 500 } });
+    console.log(books);
+}
+// ex1()
 
 //2. Find books whose rating is less than 5, and sort by the author's name
+async function ex2() {
+    const books = await Book
+        .find({ rating: { $lt: 5 } })
+        .sort({ author: 1 })
+        .exec();
+    console.log(books);
+}
+// ex2();
 
 //3. Find all the Fiction books, skip the first 2, and display only 3 of them
-
+async function ex3() {
+    const books = await Book
+        .find({ genres: 'Fiction' })
+        .skip(2)
+        .limit(3)
+        .exec();
+    console.log(books);
+}
+// ex3();
 
 /*People
 ----------------------*/
 //1. Find all the people who are tall (>180) AND rich (>30000)
+async function ex1() {
+    const people = await Person.count({
+        $and: [
+            { height: { $gt: 180 } },
+            { salary: { $gt: 30000 } }
+        ]
+    })
+    console.log(people)
+}
+// ex1()
 
 //2. Find all the people who are tall (>180) OR rich (>30000)
+async function ex2() {
+    const people = await Person.count({
+        $or: [
+            { height: { $gt: 180 } },
+            { salary: { $gt: 30000 } }
+        ]
+    })
+    console.log(people)
+}
+// ex2()
 
 //3. Find all the people who have grey hair or eyes, and are skinny (<70)
+async function ex3() {
+    const people = await Person.find().and([
+        { $or: [{ hair: 'grey' }, { eyes: 'grey' }] },
+        { weight: { $lte: 60 } }
+    ])
+    console.dir(people, { depth: null });
+}
+// ex3()
+
 
 //4. Find people who have at least 1 kid with grey hair
+async function ex4() {
+    // const people = await Person.count({'kids.hair': 'grey'})
+    const people = await Person.count().and([
+        { 'kids.hair': 'grey' },
+        { kids: { $size: 2 } }
+    ])
+    console.log(people);
+}
+// ex4()
 
 //5. Find all the people who have at least one overweight kid, and are overweight themselves (>100)
+async function ex5() {
+    const people = await Person.find({}, { weight: 1, kids: 1 }).and([
+        { 'kids.weight': { $gt: 60 } },
+        { weight: { $gt: 100 } }
+    ])
+    console.dir(people, { depth: null });
+}
+ex5()
